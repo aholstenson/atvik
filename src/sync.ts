714fd65f89bf407a9d35aadfff70156dc55dc492
Andreas Holstenson <a@holstenson.se>
2019-07-13 11:18:47 +0200

    refactor: Breaking out Listener and Subscribable to separate files

--- src/sync.ts
+++ src/sync.ts
@@ -1,41 +1,6 @@
 import { SubscriptionHandle } from './handle';
-
-/**
- * Listener type. Defines the function signature that a listener is expected
- * to have.
- */
-export type Listener<This, Args extends any[]> = (this: This, ...args: Args) => void;
-
-/**
- * Functions used to subscribe and unsubscribe to an event.
- */
-export interface Subscribable<This, Args extends any[]> {
-	/**
-	 * Subscribe to the event, will invoke the given function when the event
-	 * is emitted.
-	 */
-	(listener: Listener<This, Args>): SubscriptionHandle;
-
-	/**
-	 * Subscribe to the event, will invoke the given function when the event
-	 * is emitted.
-	 */
-	subscribe(listener: Listener<This, Args>): SubscriptionHandle;
-
-	/**
-	 * Unsubscribe a previously subscribed listener.
-	 *
-	 * @param listener
-	 */
-	unsubscribe(listener: Listener<This, Args>): boolean;
-
-	/**
-	 * Subscribe to an event but only trigger the listener once.
-	 *
-	 * @param listener
-	 */
-	once(): Promise<Args>;
-}
+import { Listener } from './listener';
+import { Subscribable } from './subscribable';
 
 /**
  * An event that fires its listeners in a synchronous fashion.
