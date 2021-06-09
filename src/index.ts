73e35c71e84c7b32a61b467133a51b37dc2a8e00
Andreas Holstenson <a@holstenson.se>
2021-06-09 10:36:44 +0200

    feat: createSubscribable is now part of the public API

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 export * from './Listener';
+
+export * from './createSubscribable';
 export * from './Subscribable';
+export * from './SubscriptionFunctions';
 export * from './SubscriptionHandle';
 export * from './Event';
